08c1f3302cd42736160f47efe7b98992c5d75417
Massey Isa'ako <massey@silverstripe.com>
2019-05-20 10:15:07 +1200

    feature/vue-refactor: initial refactor to Vue framework.

--- js/script.js
+++ js/script.js
@@ -1,165 +1,184 @@
-google.charts.load('current', {
-	'packages': 
-		[
-			'line'
-		]
-	}
-);
 
-google.charts.setOnLoadCallback(drawChart);
+var app = new Vue({
+	el: '#app',
+	data: {
+		weightData: [],
+		packGoal: [],
+		toggleOverlay: false,
+		buttonText: 'More info'
+	},
+	mounted: function() {
+		this.setWeightData();
+		this.initMap();
+		this.doPopulatePackWeightText();
+	},
+	methods: {
 
-// Contains all the data per person. Day is the axis, the other strings in the initial array describe the individual
-// lines on the graph. The integers are the values that are being plotted
-var recordedWeightData = [
-	['Day', 'Taase', 'Valasi', 'Manu', 'Cochise', 'Thomas', 'Tumema', 'Lasi', 'Freida', 'Annie', 'Baseline'],
-	[0,		115,	  118,		126,	 153,	    112,	 131,	   112,	   126, 	 104,	 80],
-	[1,		115,	  118,		126,	 153,	    112,	 131,	   112,	   126, 	 104,	 80],
-	// [5,		110,	  115,		118,	 147,	    108,	 126,	   108,	   120, 	 100,	 80]
-];
+		/**
+		 * Set data
+		 */
+		setWeightData() {
+			// Contains all the data per person. Day is the axis, the other strings in the initial array describe the individual
+			// lines on the graph. The integers are the values that are being plotted
+			var recordedWeightData = [
+				['Day', 'Taase', 'Valasi', 'Manu', 'Cochise', 'Thomas', 'Tumema', 'Lasi', 'Freida', 'Annie', 'Baseline'],
+				[0,		115,	  118,		126,	 153,	    112,	 131,	   112,	   126, 	 104,	 80],
+				[1,		115,	  118,		126,	 153,	    112,	 131,	   112,	   126, 	 104,	 80],
+				// [5,		110,	  115,		118,	 147,	    108,	 126,	   108,	   120, 	 100,	 80]
+			];
 
-var goalWeightData = [
-	90, 	// Taase
-	100,	// Valasi
-	95,		// Manu
-	110,	// Cochise
-	105,	// Thomas
-	100,	// Tumema
-	90,		// Lasi
-	95,		// Freida
-	90		// Annie
-];
+			// Our goal weight
+			var goalWeightData = [
+				90, 	// Taase
+				100,	// Valasi
+				95,		// Manu
+				110,	// Cochise
+				105,	// Thomas
+				100,	// Tumema
+				90,		// Lasi
+				95,		// Freida
+				90		// Annie
+			];
 
-// Toggling the overlay using this boolean
-var toggleOverlay = false;
+			// Set data
+			this.weightData = recordedWeightData;
+			this.packGoal = goalWeightData;
+		},
 
-// Add the event listener (click) for the toggling of the more information
-document.querySelector('#chart__more-info').addEventListener('click', toggle);
+		/**
+		 * Initialise the google map
+		 * 
+		 * @return void
+		 */
+		initMap() {
+			google.charts.load('current', {
+				'packages': 
+					[
+						'line'
+					]
+				}
+			);
 
-// Start date
-var startDate = '20/4/2019';
+			google.charts.setOnLoadCallback(this.drawChart);
+		},
 
-/**
- * Draw the Chart
- * 
- * @return void
- */
-function drawChart() {
-	var data = google.visualization.arrayToDataTable(recordedWeightData);
+		/**
+		 * Draw the Chart
+		 * 
+		 * @return void
+		 */
+		drawChart() {
+			var data = google.visualization.arrayToDataTable(this.weightData);
+			var options = {
+		      curveType: 'function',
+		      legend: {
+		      	position: 'bottom'
+		      },
+		    };
 
-	var options = {
-      curveType: 'function',
-      legend: {
-      	position: 'bottom'
-      },
-    };
+			var chart = new google.charts.Line(document.querySelector('#chart__element'));
 
-	var chart = new google.charts.Line(document.querySelector('#chart__element'));
+			chart.draw(data, google.charts.Line.convertOptions(options));
+		},
 
-	chart.draw(data, google.charts.Line.convertOptions(options));
-}
+		/**
+		 * Toggle function for the more information window pane
+		 * 
+		 * @return void
+		 */
+		toggle() {
+			var button = document.querySelector('#chart__more-info');
 
-/**
- * Toggle function for the more information window pane
- * 
- * @return void
- */
-function toggle() {
-	// The overlay element in the DOM
-	var overlay = document.querySelector('.overlay');
+			if (this.toggleOverlay) {
+				this.buttonText = 'More info';
+			} else {
+				this.buttonText = 'Less info';
+			}
 
-	if (!toggleOverlay) {
-		var button = document.querySelector('#chart__more-info');
-		
-		// Start date data
-		var startTextElement = document.querySelector('#start-date');
+			this.toggleOverlay = !this.toggleOverlay;
+		},
 
-		// Current date data
-		var currentDate = new Date();
+		/**
+		 * Populate the dropdown overlay with pack weight text
+		 * 
+		 * @return void
+		 */
+		doPopulatePackWeightText() {	
+			// Start date data
+			var startTextElement = document.querySelector('#start-date');
+			var startDate = '20/5/2019';
 
-		var currentDateTextElement = document.querySelector('#current-date');
+			// Date data
+			var currentDate = new Date();
+			var currentDateTextElement = document.querySelector('#current-date');
 
-		// Current pack weight data
-		var currentPackWeightText = document.querySelector('#pack-weight-current');
-		var packWeightCurrent = getPackWeightCurrent();
+			// Current pack weight data
+			var currentPackWeightText = document.querySelector('#pack-weight-current');
 
-		// Goal pack weight data
-		var goalWeightText = document.querySelector('#pack-weight-goal');
-		var packWeightGoal = getPackWeightGoal();
+			// Goal pack weight data
+			var goalWeightText = document.querySelector('#pack-weight-goal');
 
-		// Pack weight difference between current and goal
-		var diffWeightText = document.querySelector('#pack-weight-difference');
-		var packWeightDiff = getPackWeightDifference();
+			// Pack weight difference between current and goal
+			var diffWeightText = document.querySelector('#pack-weight-difference');
 
-		// Toggle the dropdown
-		overlay.classList.add('overlay--open');
+			// Weight text
+			currentPackWeightText.innerHTML = `${this.getPackWeightCurrent()}<span class="chart__text chart__text--small">kg</span>`;
+			goalWeightText.innerHTML = `${this.getPackWeightGoal()}<span class="chart__text chart__text--small">kg</span>`;
+			diffWeightText.innerHTML = `${this.getPackWeightDifference()}<span class="chart__text chart__text--small">kg</span>`;
 
-		// Set information
-		button.innerText = 'Less info';
+			// Date text
+			startTextElement.innerText = `${startDate}`
+			currentDateTextElement.innerText = `${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getYear() + 1900}`;
+		},
+			
+		/**
+		 * Get the combined weight of all participants
+		 * 
+		 * @return integer
+		 */
+		getPackWeightCurrent() {
+			// Retrieve last entry in  arraylist
+			var weightData = this.weightData;
+			var latestData = weightData.pop();
+			var weightTotal = 0;
 
-		// Weight text
-		currentPackWeightText.innerHTML = `${packWeightCurrent}<span class="chart__text chart__text--small">kg</span>`
-		goalWeightText.innerHTML = `${packWeightGoal}<span class="chart__text chart__text--small">kg</span>`;
-		diffWeightText.innerHTML = `${packWeightDiff}<span class="chart__text chart__text--small">kg</span>`;
+			// Sanitise the data
+			latestData.pop();
+			latestData.shift();
 
-		// Date text
-		startTextElement.innerText = `${startDate}`
-		currentDateTextElement.innerText = `${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getYear() + 1900}`;
-	} else {
-		overlay.classList.remove('overlay--open');
+			latestData.forEach(function (item) {
+				weightTotal += item;
+			});
 
-		// Wait 0.2s before reloading page
-		setTimeout(function() {
-			location.reload();
-		}, 200);
-	}
+			return weightTotal;
+		},
+			
+		/**
+		 * Retrieve the goal weight
+		 * 
+		 * @return integer
+		 */
+		getPackWeightGoal() {
+			var total = 0;
+			var goalWeightData = this.packGoal;
 
-	toggleOverlay = !toggleOverlay;
-}
-	
-/**
- * Get the combined weight of all participants
- * 
- * @return integer
- */
-function getPackWeightCurrent() {
-	// Retrieve last entry in recordedWeightData arraylist
-	var latestData = recordedWeightData.pop();
-	var weightTotal = 0;
+			goalWeightData.forEach(function(item) {
+				total += item;
+			});
 
-	// Sanitise the data
-	latestData.pop();
-	latestData.shift();
+			return total;
+		},
 
-	latestData.forEach(function (item) {
-		weightTotal += item;
-	});
+		/**
+		 * Get the different between the current pack weight and the goal pack weight.
+		 * 
+		 * @return integer
+		 */
+		getPackWeightDifference() {
+			var currentPackWeight = this.getPackWeightCurrent();
+			var goalPackWeight = this.getPackWeightGoal();
 
-	return weightTotal;
-}
-	
-/**
- * Retrieve the goal weight
- * 
- * @return integer
- */
-function getPackWeightGoal() {
-	var total = 0;
-
-	goalWeightData.forEach(function(item) {
-		total += item;
-	});
-
-	return total;
-}
-
-/**
- * Get the different between the current pack weight and the goal pack weight.
- * 
- * @return integer
- */
-function getPackWeightDifference() {
-	var currentPackWeight = getPackWeightCurrent();
-	var goalPackWeight = getPackWeightGoal();
-
-	return currentPackWeight - goalPackWeight;
-}
+			return currentPackWeight - goalPackWeight;
+		}
+	},
+});
